a5181d96b998fe34fc7a1694064067741c84a9c1
pavankalyan1302 <69458192+pavankalyan1302@users.noreply.github.com>
2020-08-11 13:59:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,35 @@
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/img/a.jgp',
+  '/img/biga.jgp',
+  '/img/ba.jgp',
+  '/img/back.jgp',
+];
+
 // install event
 self.addEventListener('install', evt => {
-  console.log('service worker installed');
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open('site-static').then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker activated');
+  //console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
